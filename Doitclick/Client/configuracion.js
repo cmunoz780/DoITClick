c3117cb06d012d9df7c1dde1772ea8216719c48c
Carlos Pradenas <carlpradenas@gmail.com>
2018-09-06 19:08:56 -0300

    Correcciones de merge

--- Doitclick/Client/configuracion.js
+++ Doitclick/Client/configuracion.js
@@ -3,15 +3,6 @@
     urlBase: '/'
 };
 
-
-<<<<<<< HEAD
 $.ajaxSetup({
     headers: { 'Authorization': 'Bearer ' + localStorage.getItem("token") }
 });
-=======
-
-$.ajaxSetup({
-    headers: { 'Authorization': 'Bearer ' + localStorage.getItem("token") }
-});
-
->>>>>>> dbabae7f9808b82ea8e6b3e97a47f4b29332d943
